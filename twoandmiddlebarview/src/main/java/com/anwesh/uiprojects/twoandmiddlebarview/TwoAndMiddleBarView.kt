97e57e007eb644fbb1d9b3697e82491a1fe8c125
Anwesh43 <anweshthecool0@gmail.com>
2020-06-28 14:36:23 +0530

    adding TwoAndMiddleBarView class

--- twoandmiddlebarview/src/main/java/com/anwesh/uiprojects/twoandmiddlebarview/TwoAndMiddleBarView.kt
+++ twoandmiddlebarview/src/main/java/com/anwesh/uiprojects/twoandmiddlebarview/TwoAndMiddleBarView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawTAMBNode(i : Int, scale : Float, paint : Paint) {
     drawTwoAndMiddleBar(scale, w, h, paint)
     restore()
 }
+
+class TwoAndMiddleBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
